135cdd6cc64d0165a11f496670ebd85070e670ab
Marcos G. Zimmermann <mgzmaster@gmail.com>
2020-12-01 14:08:58 -0300

    Add char.js to the dashboard

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,6 +16,7 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import NProgress from "nprogress"
 import {LiveSocket} from "phoenix_live_view"
+import "./dashboard/charts.js";
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
